f1dbfccbe484a2d0a4a27329b934241c8fbe7186
Amir Hadifar <Hadifar.Amir@Gmail.com>
2018-07-23 17:47:52 +0430

    Update config.py

--- models/config.py
+++ models/config.py
@@ -7,8 +7,8 @@ MAX_KEEP_WORD = 70000
 MAX_VOCAB_SIZE = 70005
 
 BATCH_SIZE = 64
-EPOCH_NUM = 10
-STEPS_PER_EPOCH = 1000
+EPOCH_NUM = 25
+STEPS_PER_EPOCH = 15000
 
 WORD_EMBEDDING_DIM = 300
 EMBEDDING_CACHE = '/Users/mac/PycharmProjects/riminder/data/embedding/embedding_matrix.npy'
